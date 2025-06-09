af0af394f3879bf5fe9d332079b5ac4c441de8eb
Vineeth Varadi <vineethv@asakta.com>
2025-06-10 00:51:05 +0530

    fix

--- Angular - Go/ui/src/app/app-routing.module.ts
+++ Angular - Go/ui/src/app/app-routing.module.ts
@@ -1,7 +1,41 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {LoginComponent} from "./components/login/login.component";
+import {HomeComponent} from "./components/home/home.component";
+import {CalenderComponent} from "./components/calender/calender.component";
+import {TodoComponent} from "./components/todo/todo.component";
+import {UploadComponent} from "./components/upload/upload.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'login',
+    pathMatch: 'full'
+  },
+  {
+    path: 'login',
+    component:LoginComponent
+  },
+  {
+    path: 'home',
+    component:HomeComponent
+  }
+  ,
+  {
+    path: 'calender',
+    component:CalenderComponent
+  }
+  ,
+  {
+    path: 'todo',
+    component:TodoComponent
+  }
+  ,
+  {
+    path: 'upload',
+    component:UploadComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- Angular - Go/ui/src/app/app.module.ts
+++ Angular - Go/ui/src/app/app.module.ts
@@ -4,11 +4,23 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './components/login/login.component';
+import { HomeComponent } from './components/home/home.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { CalenderComponent } from './components/calender/calender.component';
+import { TodoComponent } from './components/todo/todo.component';
+import { UploadComponent } from './components/upload/upload.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponent
+    LoginComponent,
+    HomeComponent,
+    HeaderComponent,
+    FooterComponent,
+    CalenderComponent,
+    TodoComponent,
+    UploadComponent
   ],
   imports: [
     BrowserModule,
